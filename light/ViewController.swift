09d639c0ab8d5e8f13a84980754052e872b4fc3c
Ian Bourke <ian.t.bourke@gmail.com>
2018-11-13 16:21:27 -0500

    Initial Commit

---
+++ light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  light
+//
+//  Created by Ian Bourke on 11/13/18.
+//  Copyright © 2018 Ian Bourke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
